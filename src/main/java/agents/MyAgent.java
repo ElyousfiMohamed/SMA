90f6a00cf36f00c8f433527679ae4bf109bd00bb
ELYOUSFI Mohamed <mohamed5elyousfi@gmail.com>
2022-06-03 11:38:44 +0100

    Add behaviors

--- src/main/java/agents/MyAgent.java
+++ src/main/java/agents/MyAgent.java
@@ -2,19 +2,68 @@ package agents;
 
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.core.behaviours.ParallelBehaviour;
+import jade.core.behaviours.TickerBehaviour;
 
 public class MyAgent extends Agent {
 
+    private int compteur = 0;
+
     @Override
     protected void setup() {
-        addBehaviour(new Behaviour() {
+        ParallelBehaviour parallelBehaviour = new ParallelBehaviour();
+        addBehaviour(parallelBehaviour);
+
+       /* parallelBehaviour.addSubBehaviour(new TickerBehaviour(this, 1000) {
+            @Override
+            protected void onTick() {
+                System.out.println("Tick "+(++compteur));
+            }
+        });
+
+        parallelBehaviour.addSubBehaviour(new Behaviour() {
             @Override
             public void action() {
-                System.out.println("Action...");
+                System.out.println("Action..."+(++compteur));
             }
 
             @Override
             public boolean done() {
+                if(compteur > 1000) {
+                    System.out.println("Done");
+                    return true;
+                }
+                return false;
+            }
+        });*/
+
+        addBehaviour(new OneShotBehaviour() {
+            @Override
+            public void action() {
+                System.out.println("One Shot Behaviour");
+            }
+        });
+
+        addBehaviour(new TickerBehaviour(this, 1000) {
+            @Override
+            protected void onTick() {
+                System.out.println("Tick "+(++compteur));
+            }
+        });
+
+        addBehaviour(new Behaviour() {
+            @Override
+            public void action() {
+                System.out.println("Action..."+(++compteur));
+            }
+
+            @Override
+            public boolean done() {
+                if(compteur > 1000) {
+                    System.out.println("Done");
+                    return true;
+                }
                 return false;
             }
         });
